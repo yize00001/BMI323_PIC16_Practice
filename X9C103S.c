a36c67f38504639de470160355ebe1176bc456db
yize00001 <yize00001@gmail.com>
2026-05-22 09:27:54 +0800

    Add X9C103S digital potentiometer driver and UART control
    - Add X9C103S.c/h: PIC16 GPIO-based driver (CS/INC/U/D on RC4/RC6/RC7)
    - Init synchronizes to position 0 via 100 step-down pulses
    - UART commands: + (step up), - (step down), p## (goto position 0-99)
    - MCC: add RC4/RC6/RC7 as GPIO output with Start High

---
+++ X9C103S.c
@@ -0,0 +1,70 @@
+#include "X9C103S.h"
+
+static uint8_t current_position = 0;
+
+void x9c103s_init(void)
+{
+    X9C_CS_High();
+    X9C_INC_High();
+    X9C_UD_High();
+    __delay_ms(1);
+
+    // Synchronize to position 0: send 100 step-down pulses
+    uint8_t i;
+    X9C_UD_Low();
+    for (i = 0; i < 100; i++) {
+        X9C_CS_Low();
+        X9C_INC_Low();
+        __delay_us(10);
+        X9C_INC_High();
+        __delay_us(10);
+        X9C_CS_High();
+    }
+    current_position = 0;
+    printf("X9C103S init OK, pos=0\r\n");
+}
+
+void x9c103s_step_up(void)
+{
+    if (current_position >= X9C_MAX_POS) return;
+    X9C_UD_High();
+    X9C_CS_Low();
+    X9C_INC_Low();
+    __delay_us(10);
+    X9C_INC_High();
+    __delay_us(10);
+    X9C_CS_High();
+    current_position++;
+}
+
+void x9c103s_step_down(void)
+{
+    if (current_position <= X9C_MIN_POS) return;
+    X9C_UD_Low();
+    X9C_CS_Low();
+    X9C_INC_Low();
+    __delay_us(10);
+    X9C_INC_High();
+    __delay_us(10);
+    X9C_CS_High();
+    current_position--;
+}
+
+void x9c103s_goto_position(uint8_t target)
+{
+    if (target > X9C_MAX_POS) target = X9C_MAX_POS;
+    while (current_position != target) {
+        if (current_position < target)
+            x9c103s_step_up();
+        else
+            x9c103s_step_down();
+    }
+    printf("X9C pos=%d (~%d ohm)\r\n",
+           current_position,
+           (int)((uint32_t)current_position * 10000U / 99U));
+}
+
+uint8_t x9c103s_get_position(void)
+{
+    return current_position;
+}
